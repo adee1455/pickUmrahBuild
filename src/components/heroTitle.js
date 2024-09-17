b4bf469ca9abc332a2e0fb4d790cfe1c901fb191
Adee Shaikh <shaikhadee1455@gmail.com>
2024-09-17 18:02:52 +0530

    changes

--- src/components/heroTitle.js
+++ src/components/heroTitle.js
@@ -6,8 +6,9 @@ export default function Title() {
 
 return(
 
-    <div className="bg-white flex justify-center border-[#E6D096] border-y-[1px] " >
-        <span className="text-black sm:leading-0 md:leading-3 lg:leading-5 font-extrabold sm:p-10 p-4  text-md lg:text-4xl" >We help you Compare & Pick the best Umrah Package for You</span>
+    <div className="bg-white flex flex-col justify-center border-[#E6D096] border-y-[1px] " >
+        <span className="text-black sm:text-center text-left sm:leading-0 md:leading-3 lg:leading-5 font-extrabold sm:pt-8 pt-4 p-3 pb-1  text-md lg:text-4xl" >Find & Pick Umrah package according to your convenience</span> 
+        <span className="text-black sm:text-center text-left sm:leading-0 md:leading-3 lg:leading-5 font-semibold sm:pb-5 sm:pt-3 pt-0  p-3  text-xs lg:text-xl" >We list Umrah and Hajj Packages from different tour operators</span>
     </div>
 
 )};
