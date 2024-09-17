4f626ebfddd5d8859327521ce57e65f2afec1512
Adee Shaikh <shaikhadee1455@gmail.com>
2024-09-17 18:09:52 +0530

    s

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
-
+import { Analytics } from "@vercel/analytics/react"
 export const metadata = {
   title: "PickUmrah",
   description: "PickUmrah",
@@ -11,6 +11,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <SpeedInsights/>
+      <Analytics/>
       <body className={inter.className}>{children}</body>
     </html>
   );
