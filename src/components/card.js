0dca363473c38884379dd21d8945bbfdad5703a2
adee1455 <shaikhadee1455@gmail.com>
2024-12-07 13:15:51 +0530

    changes

--- src/components/card.js
+++ src/components/card.js
@@ -13,7 +13,7 @@ import { LikeButton } from '@lyket/react';
 
 
 
-export default function Card({tourName,tourCity,departDate,arrivalDate,location,price,days,type,btnLink,makkahHotel,madinahHotel,makkahHotel2,madinahHotel2,stars,makkahDist, madinahDist}){
+export default function Card({tourName,tourCity,ratings,departDate,arrivalDate,location,price,days,type,btnLink,makkahHotel,madinahHotel,makkahHotel2,madinahHotel2,stars,makkahDist, madinahDist,Makkahhoteltour,Madinahhoteltour}){
 
     const router = useRouter();
     const link = btnLink;
@@ -33,7 +33,7 @@ export default function Card({tourName,tourCity,departDate,arrivalDate,location,
                           <div id="div7" className="pl-4 sm:pr-52 pr-2 py-2 ">
                                 <h2 className="font-bold sm:text-2xl text-xl">{tourName}</h2>
                                 <span className="sm:text-lg text-md text-center">{tourCity}</span>
-                               <span className="flex flex-row sm:w-72 w-auto  sm:text-md text-md  ">Tour Ratings: <div className="flex sm:pt-0 sm:pl-2 pt-[2px]"><svg className="sm:h-6 h-5 p-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#444444" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg><svg className="sm:h-6 h-5 py-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#444444" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg><svg className="sm:h-6 h-5 py-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#444444" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg><svg className="sm:h-6 h-5 py-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#444444" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg><svg className="sm:h-6 h-5 p-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/></svg></div></span>
+                               <span className="flex flex-row sm:w-72 w-auto  sm:text-md text-md  ">Tour Ratings: {ratings}★</span>
 
                             </div>
                          
@@ -80,14 +80,15 @@ export default function Card({tourName,tourCity,departDate,arrivalDate,location,
                             </div>
 
                      </div>
-                      
+                     {/* {makkahHotel} <br /> {makkahHotel2} */} 
+                     {/* {madinahHotel} <br /> {madinahHotel2}  */}
 
                 </div>
 
                 <div id="rightMain" className="p-4 lg:flex md:hidden flex-row lg:visible hidden   ">
                      <div id="div13" className="flex flex-col pt-2">
-                        <span className="text-center text-lg flex items-center"><img src="hotel.svg" className="pr-3 h-8" alt="" srcset="" /> <span className="pt-1">Hotel in Makkah</span></span>
-                        <span className="text-center pt-3 font-bold text-2xl ">{makkahHotel} <br /> {makkahHotel2}</span>
+                        <span className="text-center text-lg flex items-center ml-5"><img src="hotel.svg" className="pr-3 h-8" alt="" srcset="" /> <span className="pt-1">Hotel in Makkah</span></span>
+                        <span className="text-center pt-3 font-bold text-2xl w-64">{Makkahhoteltour}</span>
                         <span className="text-center pt-4 text-lg font-bold flex items-center justify-center"><svg className="h-4 pr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#444444" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg> {stars} star</span>
                         <span className="text-center pt-5">Distance from Haram: <span className="font-semibold">{makkahDist}</span></span>
 
@@ -98,8 +99,8 @@ export default function Card({tourName,tourCity,departDate,arrivalDate,location,
                      </div>
 
                      <div id="div14" className="flex flex-col pt-2">
-                     <span className="text-center text-lg flex items-center"><img src="hotel.svg" className="pr-3 h-8" alt="" srcset="" /> <span className="pt-1">Hotel in Madinah</span></span>
-                        <span className="text-center pt-3 font-bold text-2xl ">{madinahHotel} <br /> {madinahHotel2} </span>
+                     <span className="text-center text-lg flex items-center ml-2"><img src="hotel.svg" className="pr-3 h-8" alt="" srcset="" /> <span className="pt-1">Hotel in Madinah</span></span>
+                        <span className="text-center pt-3 font-bold text-2xl w-52">{Madinahhoteltour}</span>
                         <span className="text-center pt-4 text-lg font-bold flex items-center justify-center"><svg className="h-4 pr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#444444" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg> {stars} star</span>
                         <span className="text-center pt-5">Distance from Haram: <span className="font-semibold">{madinahDist}</span></span>
 
